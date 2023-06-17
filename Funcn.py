21ed85d67e811cbef855cf6706fed9e2fdb2b7a0
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-06-17 15:45:33 +0530

    Create Funcn.py

---
+++ Funcn.py
@@ -0,0 +1,94 @@
+
+import os
+import subprocess
+import math
+import time
+import asyncio
+import aiohttp
+from datetime import datetime as dt
+import psutil
+import signal
+from telegraph import TelegraphPoster
+
+from . import *
+from .config import *
+
+WORKING = []
+QUEUE = {}
+OK = {}
+
+uptime = dt.now()
+os.system(f"wget {THUMB} -O thumb.jpg")
+
+if not os.path.isdir("downloads/"):
+    os.mkdir("downloads/")
+if not os.path.isdir("encode/"):
+    os.mkdir("encode/")
+if not os.path.isdir("thumb/"):
+    os.mkdir("thumb/")
+
+
+def stdr(seconds: int) -> str:
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    if len(str(minutes)) == 1:
+        minutes = "0" + str(minutes)
+    if len(str(hours)) == 1:
+        hours = "0" + str(hours)
+    if len(str(seconds)) == 1:
+        seconds = "0" + str(seconds)
+    dur = (
+        ((str(hours) + ":") if hours else "00:")
+        + ((str(minutes) + ":") if minutes else "00:")
+        + ((str(seconds)) if seconds else "")
+    )
+    return dur
+
+
+def ts(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + "d, ") if days else "")
+        + ((str(hours) + "h, ") if hours else "")
+        + ((str(minutes) + "m, ") if minutes else "")
+        + ((str(seconds) + "s, ") if seconds else "")
+        + ((str(milliseconds) + "ms, ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+def hbs(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "B", 1: "K", 2: "M", 3: "G", 4: "T", 5: "P"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+No_Flood = {}
+
+
+async def progress(current, total, event, start, type_of_ps, file=None):
+    now = time.time()
+    if No_Flood.get(event.chat_id):
+        if No_Flood[event.chat_id].get(event.id):
+            if (now - No_Flood[event.chat_id][event.id]) < 1.1:
+                return
+        else:
+            No_Flood[event.chat_id].update({event.id: now})
+    else:
+        No_Flood.update({event.chat_id: {event.id: now}})
+    diff = time.time() - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        time_to_completion = round((total - current) / speed) * 1000
+        progress_str = "`[{0}{1}] {2}%`\n\n".format(
+            "".join(["●" for i in range(math.floor(percentage / 
